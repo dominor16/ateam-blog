b5616a6cfaf8e823e1916e7ce821d5195776c581
Ryoma <ryoma@planningdev.com>
2016-11-29 18:26:29 +0900

    blogが表示される順序がバラバラになるバグを改修

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,7 +4,7 @@ class BlogsController < ApplicationController
   # GET /blogs
   # GET /blogs.json
   def index
-    @blogs = Blog.all.reverse
+    @blogs = Blog.order(:created_at).reverse
     @blog = Blog.new
   end
 
